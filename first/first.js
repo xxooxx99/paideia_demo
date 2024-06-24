124a3acf82df0cfbb98267ae1d80d593a9f238a7
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-06-24 11:32:58 +0900

    test front

--- first/first.js
+++ first/first.js
@@ -1,19 +1,19 @@
 //성인과 학생 페이지를 따로 만들어야 하는지
-document.addEventListener("DOMContentLoaded", () => {
-  const studentBtn = document.getElementById("studentBtn");
-  const adultBtn = document.getElementById("adultBtn");
+document.addEventListener('DOMContentLoaded', () => {
+  const studentBtn = document.getElementById('studentBtn');
+  const adultBtn = document.getElementById('adultBtn');
 
   if (studentBtn) {
-    studentBtn.addEventListener("click", (e) => {
+    studentBtn.addEventListener('click', (e) => {
       e.preventDefault();
-      window.location.href = "/front/paideia/paideia.html?type=student";
+      window.location.href = '/paideia/paideia.html?type=student';
     });
   }
 
   if (adultBtn) {
-    adultBtn.addEventListener("click", (e) => {
+    adultBtn.addEventListener('click', (e) => {
       e.preventDefault();
-      window.location.href = "/front/paideia/paideia.html?type=adult";
+      window.location.href = '/paideia/paideia.html?type=adult';
     });
   }
 });
--- graph/graph.js
+++ graph/graph.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const userNameDisplay = document.getElementById("userNameDisplay");
-  const savedName = localStorage.getItem("userName");
+document.addEventListener('DOMContentLoaded', () => {
+  const userNameDisplay = document.getElementById('userNameDisplay');
+  const savedName = localStorage.getItem('userName');
   if (savedName) {
     userNameDisplay.textContent = savedName;
   }
 
   const buttons = {
-    homeButton: "/front/paideia/paideia.html",
-    graphButton: "/front/graph/graph.html",
-    progressButton: "/front/progress/progress.html",
-    settingButton: "/front/set/setting.html",
+    homeButton: '/paideia/paideia.html',
+    graphButton: '/graph/graph.html',
+    progressButton: '/progress/progress.html',
+    settingButton: '/set/setting.html',
   };
 
   for (let [id, url] of Object.entries(buttons)) {
     const button = document.getElementById(id);
     if (button) {
-      button.addEventListener("click", (e) => {
+      button.addEventListener('click', (e) => {
         e.preventDefault();
         window.location.href = url;
       });
